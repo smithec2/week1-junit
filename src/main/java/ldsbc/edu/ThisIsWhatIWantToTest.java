31ac3862559e5e9194ea4704f89a1256cc5fca67
Craig Smith <csmith@ereinsure.com>
2017-01-10 17:43:44 -0700

    First unit test

---
+++ src/main/java/ldsbc/edu/ThisIsWhatIWantToTest.java
@@ -0,0 +1,12 @@
+package ldsbc.edu;
+
+/**
+ * Created by CSmith on 1/10/2017.
+ */
+public class ThisIsWhatIWantToTest {
+  
+  public String getHello(){
+    return "Hello";
+  }
+  
+}
---
+++ src/test/java/ldsbc/edu/ThisIsWhatIWantToTestTest.java
@@ -0,0 +1,16 @@
+package ldsbc.edu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by CSmith on 1/10/2017.
+ */
+public class ThisIsWhatIWantToTestTest {
+  @Test
+  public void getHello() throws Exception {
+    
+  }
+  
+}
